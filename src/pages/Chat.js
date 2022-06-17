62aa917174f0eb309f4dd34d65f5b704b5a952d7
munilin <munilin@naver.com>
2022-06-17 20:44:38 +0900

    테스트

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import styled from "styled-components";
+
 
 const Chat = (props) => {
   
   return (
     <React.Fragment>
-      <p>hi Chat</p>
+      <TopBar>
+      <p>검색창</p>
+      </TopBar>
     </React.Fragment>
   );
 }
 
+const TopBar = styled.div`
+  width: 100wh;
+  height: 30px;
+  margin: auto;
+  padding: 16px;
+  border: 2px solid;
+  border-radius: 5px;
+  display: flex;
+  & input {
+    border: none;
+    margin: auto;
+    width: 100px;
+    height: 30px;
+  }
+  & span {
+    cursor: pointer;
+  }
+`;
 
 export default Chat;
