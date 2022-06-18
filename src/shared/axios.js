86ea75f8749f7227b1bfc51b92a8fa4281432572
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2022-06-19 00:05:02 +0900

    axios 관련 설정 파일 업로드

---
+++ src/shared/axios.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import { getToken } from './token';
+
+const instance = axios.create({
+  baseURL: '요청 서버 도메인',
+  headers: { 'Content-Type': 'application/json' },
+});
+
+//토큰값
+export const token = getToken();
+// instance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+export default instance;
