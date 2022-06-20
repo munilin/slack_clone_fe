ceae59310e2fa60cca9c3cef5439dc5cb71a0d6a
DongbokLee <68406448+proamateur92@users.noreply.github.com>
2022-06-21 04:01:37 +0900

    채널 삭제해서 state값 바뀌도록 redux 수정 #25

--- src/redux/modules/chatSlice.js
+++ src/redux/modules/chatSlice.js
@@ -1,26 +1,30 @@
-import instance from "../../shared/axios";
-import { createSlice } from "@reduxjs/toolkit";
-import { chatAPI } from "../../shared/api";
+import instance from '../../shared/axios';
+import { createSlice } from '@reduxjs/toolkit';
+import { chatAPI } from '../../shared/api';
 
 //redux Toolkit
 const chatSlice = createSlice({
-  name: "chat",
-  initialState: {},
+  name: 'chat',
+  initialState: {
+    list: [],
+  },
   reducers: {
     loadChatAction: (state, action) => {
-      state.data = action.payload;
+      state.list = action.payload;
     },
     postChatAction: (state, action) => {
-      state.data = action.payload;
+      state.list = action.payload;
     },
     loadChannelAction: (state, action) => {
-      state.data = action.payload;
+      state.list = action.payload;
     },
     createChannelAction: (state, action) => {
-      state.data = action.payload;
+      state.list = action.payload;
     },
     deleteChannelAction: (state, action) => {
-      state.data = action.payload;
+      console.log('삭제하기');
+      const newChannel = state.list.filter(channel => channel.id !== action.payload);
+      state.list = newChannel;
     },
   },
 });
@@ -28,18 +32,18 @@ const chatSlice = createSlice({
 // axios 가져오기
 export const loadChannel = () => {
   return function (dispatch) {
-    instance.get("http://localhost:5001/channel").then((response) => {
+    console.log('채널 가져오기');
+    instance.get('http://localhost:5001/channel').then(response => {
       dispatch(loadChannelAction(response.data));
-
       console.log(response);
     });
   };
 };
 
 // axios 추가하기
-export const createChannel = (channel) => {
+export const createChannel = channel => {
   return function (dispatch) {
-    instance.post("http://localhost:5001/channel", channel).then((response) => {
+    instance.post('http://localhost:5001/channel', channel).then(response => {
       console.log(response.data);
     });
     dispatch(createChannelAction());
@@ -47,41 +51,33 @@ export const createChannel = (channel) => {
 };
 
 // axios 삭제하기
-export const deleteChannel = (list) => {
+export const deleteChannel = list => {
   return function (dispatch) {
-    instance.delete(`http://localhost:5001/channel/${list.id}`).then((response) => {
+    instance.delete(`http://localhost:5001/channel/${list.id}`).then(response => {
       console.log(response.data);
     });
-    dispatch(deleteChannelAction());
+    dispatch(deleteChannelAction(list.id));
   };
 };
 
 // axios 가져오기
 export const loadChat = () => {
   return function (dispatch) {
-    instance.get("http://localhost:5001/chat").then((response) => {
+    instance.get('http://localhost:5001/chat').then(response => {
       dispatch(loadChatAction(response.data));
       console.log(response);
     });
   };
 };
 // axios 추가하기
-export const postChat = (chat) => {
+export const postChat = chat => {
   return function (dispatch) {
-    instance.post("http://localhost:5001/chat", chat).then((response) => {
-
+    instance.post('http://localhost:5001/chat', chat).then(response => {
       console.log(response.data);
     });
     dispatch(postChatAction());
   };
 };
 
-export const {
-  loadChatAction,
-  postChatAction,
-  createChannelAction,
-  loadChannelAction,
-  deleteChannelAction,
-} = chatSlice.actions;
+export const { loadChatAction, postChatAction, createChannelAction, loadChannelAction, deleteChannelAction } = chatSlice.actions;
 export default chatSlice.reducer;
-
